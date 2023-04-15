18a32863004bbf039926cdb4f7ef1d033386a171
Adam Peterson <ajay2828@gmail.com>
2023-04-15 18:37:30 -0500

    account for service name and config file

--- create-services.sh
+++ create-services.sh
@@ -11,18 +11,21 @@ intervals=(
 
 if [[ -n "$1" ]]; then
     name="$1-"
+    config="-c /etc/rsnapshot-$1.conf"
 fi
 
 before=""
 for i in "${intervals[@]}"; do
     fname="rsnapshot-$name$i.service"
     cp -v "$SKEL" "$fname"
-    sed -i "s/INTERVAL/$i/g" "$fname"
+    sed -i "s/{NAME}/$name/g" "$fname"
+    sed -i "s/{CONFIG}/$config/g" "$fname"
+    sed -i "s/{INTERVAL}/$i/g" "$fname"
     if [[ "$i" = "${intervals[0]}" ]]; then
         sed -i '/Requires/d' "$fname"
         sed -i '/After/d' "$fname"
     else
-        sed -i "s/BEFORE/$before/g" "$fname"
+        sed -i "s/{BEFORE}/$before/g" "$fname"
     fi
     before="$i"
 done
